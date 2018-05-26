6025e5ffa8516746151534bb1170d8c5df0a3264
dvstter <p1usj4de@163.com>
2018-05-26 17:02:11 +0800

    Update method to move to next page.

--- downloader.py
+++ downloader.py
@@ -5,27 +5,27 @@ import re
 import random
 from bs4 import BeautifulSoup
 
-Base = "http://www.sanhaoxs.org/0_234/{}.html"
-#Counter = 17281
-Counter = 158438
+Base = "http://www.sanhaoxs.org"
+Postfix = "/0_234/158748.html"
 Headers = {"user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36", 
         "cache-control":"no-cache", 
         "accept-encoding":"gzip, deflate"}
 Proxies = {"http":"http://127.0.0.1:1087", "https":"https://127.0.0.1:1087"}
 
 def get_data(hfile):
-    cnt = 1
+    next_postfix = Postfix
     while True:
-        if not request_data(cnt, hfile):
+        next_postfix = request_data(next_postfix, hfile)
+        if not next_postfix:
             break
         w_tm = random.randint(0, 5)
         print(f"Waiting for {w_tm} secs")
         time.sleep(w_tm)
-        cnt += 1
 
-def request_data(page_cnt, hfile):
-    url = Base.format(Counter + page_cnt - 1)
-    print(f"Fetching url {url}")
+def request_data(url_postfix, hfile):
+    url = Base + url_postfix
+    print("--------------------------------------------------------")
+    print(f"Fetching {url}")
     resp = requests.get(url, headers=Headers, proxies=Proxies)
     bs = BeautifulSoup(resp.text.encode("iso-8859-1").decode('gbk', "ignore"), "lxml")
     try:
@@ -35,13 +35,14 @@ def request_data(page_cnt, hfile):
         hfile.write(text)
         hfile.write("\n\n")
         hfile.flush()
-        print(f"Finish page {page_cnt}")
+        print(f"Finish current page")
 
+        next_postfix = None
         for each in bs.find_all("a"):
             if each.text == "下一章":
-                if each["href"] == "/0_234/":
-                    return False
-        return True
+                if each["href"] != "/0_234/":
+                    next_postfix = each["href"]
+        return next_postfix
     except IndexError as _:
         f = open("error_page_source.txt", "w", encoding="utf-8")
         f.write(resp.text.encode("iso-8859-1").decode("gbk"))
