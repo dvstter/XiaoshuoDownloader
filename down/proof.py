15672b83efb30ab1f2eefd3a8935011925e76cc8
chen yang <p1usj4de@gmail.com>
2022-11-25 19:06:09 +0800

    Deleted unnecessary files.

--- down/proof.py
+++ down/proof.py
@@ -2,21 +2,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-"""
-	Use this class to get beautifulsoup object, then develop the extraction and next_page method!
-"""
-class Dev:
-  Headers = {
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
-    "cache-control": "no-cache",
-    "accept-encoding": "gzip, deflate"}
-  Proxies = {"http": "http://127.0.0.1:1087", "https": "https://127.0.0.1:1087"}
+Headers = {
+  'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+  'cache-control': 'no-cache',
+  'accept-encoding': 'gzip, deflate'}
+Proxies = {'http': 'http://127.0.0.1:7890'}
 
-  @staticmethod
-  def getbs(url, gbk=False):
-    print(f"Fetching {url}")
-    resp = requests.get(url, headers=Dev.Headers, proxies=Dev.Proxies)
-    if gbk:
-      return BeautifulSoup(resp.text.encode("iso-8859-1").decode('gbk', "ignore"), "lxml")
-    else:
-      return BeautifulSoup(resp.text, "lxml")
+def get_bs(url, proxy=True, gbk=False):
+  print(f'Fetching {url}')
+  resp = requests.get(url, headers=Headers, proxies=Proxies) if proxy else requests.get(url, headers=Headers)
+  return BeautifulSoup(resp.text.encode('iso-8859-1').decode('gbk', 'ignore'), 'lxml') if gbk else BeautifulSoup(resp.text, 'lxml')
